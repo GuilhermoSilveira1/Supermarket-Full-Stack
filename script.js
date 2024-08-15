bc5c22ecddba1438d664fafa98659e603b3dae01
Guilhermo Silveira <153184158+GuilhermoSilveira1@users.noreply.github.com>
2024-08-15 12:15:57 -0300

    Update on script.js
    Merging the mainFunction.js and script.js codes.

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-// Variáveis que interagem com o DOM
-const buttons = {
-  button1: document.getElementById("button1"),
-  button2: document.getElementById("button2"),
-  button3: document.getElementById("button3"),
-  button4: document.getElementById("button4"),
-  button5: document.getElementById("button5"),
-  button6: document.getElementById("button6"),
-  button7: document.getElementById("button7"),
-};
-const subtitle1 = document.getElementById("subtitle1");
-const subtitle2 = document.getElementById("subtitle2");
-const divButtons = document.getElementById("buttons");
+document.addEventListener("DOMContentLoaded", (event) => {
+  mainPage();
+});
+
+const subtitle1 = window.document.getElementById("subtitle1");
+const subtitle2 = window.document.getElementById("subtitle2");
+const main = window.document.getElementById("conteudo");
+const divButtons = window.document.getElementById("buttons");
+
+function mainPage() {
+  subtitle1.innerHTML = "Bem Vindo!";
+  subtitle2.innerHTML = "Qual ação gostaria de realizar?";
+
+  const buttons = [
+    { text: "Realizar Venda", id: "button1" },
+    { text: "Ir para o menu cliente", id: "button2" },
+    { text: "Ir para o menu atendente", id: "button3" },
+    { text: "Consultar o histórico de compras e vendas", id: "button4" },
+    { text: "Gerenciamento de estoque", id: "button5" },
+    { text: "Pedidos de compra", id: "button6" },
+    { text: "Sair do Programa", id: "button7" },
+  ];
+
+  buttons.forEach((button) => {
+    let btn = document.createElement("button");
+    btn.textContent = button.text;
+    btn.id = button.id;
+    divButtons.appendChild(btn);
+  });
+}
 
 // Eventos de clique em cada botão
 buttons.button1.addEventListener("click", paginaVenda);
