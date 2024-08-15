3eaebee894af19072d1d6eb709213cc1abd6c13a
Guilhermo Silveira <153184158+GuilhermoSilveira1@users.noreply.github.com>
2024-08-15 12:01:29 -0300

    Add mainFunction.js
    A file containing only the main function, to load the main content of the system.

--- mainFunction.js
+++ mainFunction.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  mainPage();
+});
+
 const subtitle1 = window.document.getElementById("subtitle1");
 const subtitle2 = window.document.getElementById("subtitle2");
 const main = window.document.getElementById("conteudo");
@@ -5,29 +9,22 @@ const divButtons = window.document.getElementById("buttons");
 
 function mainPage() {
   subtitle1.innerHTML = "Bem Vindo!";
-  subtitle2.innerHTML =
-    "Qual ação gostaria de realizarQual ação gostaria de realizar?";
-  let button1 = document.createElement("button");
-  let button2 = document.createElement("button");
-  let button3 = document.createElement("button");
-  let button4 = document.createElement("button");
-  let button5 = document.createElement("button");
-  let button6 = document.createElement("button");
-  let button7 = document.createElement("button");
+  subtitle2.innerHTML = "Qual ação gostaria de realizar?";
 
-  button1.textContent = "Realizar Venda";
-  button2.textContent = "Ir para o menu cliente";
-  button3.textContent = "ir para o menu cliente";
-  button4.textContent = "Consultar o histórico de compras e vendas";
-  button5.textContent = "Gerenciamento de estoque";
-  button6.textContent = "Pedidos de compra";
-  button7.textContent = "Sair do Programa";
+  const buttons = [
+    { text: "Realizar Venda", id: "button1" },
+    { text: "Ir para o menu cliente", id: "button2" },
+    { text: "Ir para o menu atendente", id: "button3" },
+    { text: "Consultar o histórico de compras e vendas", id: "button4" },
+    { text: "Gerenciamento de estoque", id: "button5" },
+    { text: "Pedidos de compra", id: "button6" },
+    { text: "Sair do Programa", id: "button7" },
+  ];
 
-  divButtons.appendChild(button1);
-  divButtons.appendChild(button2);
-  divButtons.appendChild(button3);
-  divButtons.appendChild(button4);
-  divButtons.appendChild(button5);
-  divButtons.appendChild(button6);
-  divButtons.appendChild(button7);
+  buttons.forEach((button) => {
+    let btn = document.createElement("button");
+    btn.textContent = button.text;
+    btn.id = button.id;
+    divButtons.appendChild(btn);
+  });
 }
